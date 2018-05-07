a17f46ba085a6f55e00caeae6c533388b05645cd
Donat Ajro <donat.ajro@students.unibe.ch>
2018-05-07 18:06:59 +0200

    tried communicatin with the server and failed.

--- src/app/cases/cases.component.ts
+++ src/app/cases/cases.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule, Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+import * as jwt_decode from 'jwt-decode';
+
 
 
 @Component({
@@ -6,11 +13,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cases.component.html',
   styleUrls: ['./cases.component.css']
 })
+@Injectable()
 export class CasesComponent implements OnInit {
-
-  constructor() { }
-
+  private url: string = '/api/case';
+  private headers = new Headers({ 'Content-Type': 'application/json' })
+  private options = new RequestOptions( {headers: this.headers });
+  constructor(private http: Http) { }
+  
+  
   ngOnInit() {
+    
+    console.log(this.readCases());
+    
   }
-
+  cases=[];
+  
+  
+  
+  readCases(){
+    console.log("lsjl");
+    return this.http.get("/api/case", this.options).map((response:Response)=>{
+      console.log(response);
+      return response;
+    });
+  }
+  
 }
